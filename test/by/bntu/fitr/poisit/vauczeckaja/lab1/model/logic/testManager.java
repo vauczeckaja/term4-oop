14e933481347af10ce79391e536ec00c92bdcb53
Viktoryja Vauczeckaja <rycarzbahny@gmail.com>
2021-03-30 23:09:10 +0300

    added tests for Manager

--- test/by/bntu/fitr/poisit/vauczeckaja/lab1/model/logic/testManager.java
+++ test/by/bntu/fitr/poisit/vauczeckaja/lab1/model/logic/testManager.java
@@ -21,7 +21,15 @@ public class testManager {
 
     @Test
     public void testCalculateProfit() {
+        ArrayList<Film> films = new ArrayList<Film>(){{
+                    add(new Film("Film1", "", 10., 20.));
+                    add(new Film("Film2", "", 5., 10.));
+        }};
 
+        double expectedProfit = 15.;
+        double actualProfit = Manager.calculateProfit(films);
+
+        if (expectedProfit != actualProfit) Assert.fail();
     }
 
     @Test
@@ -35,10 +43,47 @@ public class testManager {
     }
 
     @Test
+    public void testCalculateExpenses() {
+        ArrayList<Film> films = new ArrayList<Film>(){{
+            add(new Film("Film1", "", 10., 20.));
+            add(new Film("Film2", "", 5., 10.));
+        }};
+
+        double expectedExpenses = 15.;
+        double actualExpenses = Manager.calculateProfit(films);
+
+        if (expectedExpenses != actualExpenses) Assert.fail();
+    }
+
+    @Test
     public void testCheckStatusSetIsEmpty() {
         ArrayList<Film> films = null;
 
         String expectedStatus = "";
+        String actualStatus = Manager.checkStatus(films);
+
+        if (!expectedStatus.equals(actualStatus)) Assert.fail();
+    }
+
+    @Test
+    public void testCheckStatusIsFailed() {
+        ArrayList<Film> films = new ArrayList<Film>(){{
+            add(new Film("Film1", "", 10., 5.));
+        }};
+
+        String expectedStatus = "Film1 was failed\n";
+        String actualStatus = Manager.checkStatus(films);
+
+        if (!expectedStatus.equals(actualStatus)) Assert.fail();
+    }
+
+    @Test
+    public void testCheckStatusIsSuccessful() {
+        ArrayList<Film> films = new ArrayList<Film>(){{
+            add(new Film("Film1", "", 10., 15.));
+        }};
+
+        String expectedStatus = "Film1 was successful\n";
         String actualStatus = Manager.checkStatus(films);
 
         if (!expectedStatus.equals(actualStatus)) Assert.fail();
