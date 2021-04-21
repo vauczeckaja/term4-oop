ea016ea04a7d93761fff081754076f7142f0c68d
Viktoryja Vauczeckaja <rycarzbahny@gmail.com>
2021-04-21 14:57:47 +0300

    fixed test methods

--- test/by/bntu/fitr/poisit/vauczeckaja/lab1/model/logic/testManager.java
+++ test/by/bntu/fitr/poisit/vauczeckaja/lab1/model/logic/testManager.java
@@ -1,90 +1,87 @@
-//package by.bntu.fitr.poisit.vauczeckaja.lab1.model.logic;
-//
-//import by.bntu.fitr.poisit.vauczeckaja.lab1.model.entity.content.Film;
-//
-//import org.junit.Test;
-//import org.junit.Assert;
-//import java.util.ArrayList;
-//
-//public class testManager {
-//
-//    @Test
-//    public void testCalculateProfitSetIsEmpty() {
-//        ArrayList<Film> films = null;
-//
-//        double expectedProfit = 0.0;
-//        double actualProfit = Manager.calculateProfit(films);
-//
-//        if (expectedProfit != actualProfit) Assert.fail();
-//    }
-//
-//    @Test
-//    public void testCalculateProfit() {
-//        ArrayList<Film> films = new ArrayList<Film>(){{
-//                    add(new Film("Film1", "", 10., 20.));
-//                    add(new Film("Film2", "", 5., 10.));
-//        }};
-//
-//        double expectedProfit = 15.;
-//        double actualProfit = Manager.calculateProfit(films);
-//
-//        if (expectedProfit != actualProfit) Assert.fail();
-//    }
-//
-//    @Test
-//    public void testCalculateExpensesSetIsEmpty() {
-//        ArrayList<Film> films = null;
-//
-//        double expectedExpenses = 0.0;
-//        double actualExpenses = Manager.calculateExpenses(films);
-//
-//        if (expectedExpenses != actualExpenses) Assert.fail();
-//    }
-//
-//    @Test
-//    public void testCalculateExpenses() {
-//        ArrayList<Film> films = new ArrayList<Film>(){{
-//            add(new Film("Film1", "", 10., 20.));
-//            add(new Film("Film2", "", 5., 10.));
-//        }};
-//
-//        double expectedExpenses = 15.;
-//        double actualExpenses = Manager.calculateProfit(films);
-//
-//        if (expectedExpenses != actualExpenses) Assert.fail();
-//    }
-//
-//    @Test
-//    public void testCheckStatusSetIsEmpty() {
-//        ArrayList<Film> films = null;
-//
-//        String expectedStatus = "";
-//        String actualStatus = Manager.checkStatus(films);
-//
-//        if (!expectedStatus.equals(actualStatus)) Assert.fail();
-//    }
-//
-//    @Test
-//    public void testCheckStatusIsFailed() {
-//        ArrayList<Film> films = new ArrayList<Film>(){{
-//            add(new Film("Film1", "", 10., 5.));
-//        }};
-//
-//        String expectedStatus = "Film1 was failed\n";
-//        String actualStatus = Manager.checkStatus(films);
-//
-//        if (!expectedStatus.equals(actualStatus)) Assert.fail();
-//    }
-//
-//    @Test
-//    public void testCheckStatusIsSuccessful() {
-//        ArrayList<Film> films = new ArrayList<Film>(){{
-//            add(new Film("Film1", "", 10., 15.));
-//        }};
-//
-//        String expectedStatus = "Film1 was successful\n";
-//        String actualStatus = Manager.checkStatus(films);
-//
-//        if (!expectedStatus.equals(actualStatus)) Assert.fail();
-//    }
-//}
+package by.bntu.fitr.poisit.vauczeckaja.lab1.model.logic;
+
+import by.bntu.fitr.poisit.vauczeckaja.lab1.model.entity.content.Content;
+import by.bntu.fitr.poisit.vauczeckaja.lab1.model.entity.content.Film;
+
+import org.junit.Test;
+import org.junit.Assert;
+import java.util.ArrayList;
+
+public class testManager {
+
+    @Test
+    public void testCalculateProfitSetIsEmpty() {
+        Content[] contentList = null;
+
+        int expectedProfit = 0;
+        int actualProfit = Manager.calculateProfit(contentList);
+
+        if (expectedProfit != actualProfit) Assert.fail();
+    }
+
+    @Test
+    public void testCalculateProfit() {
+        Content[] contentList = new Content[] {
+                new Film("", "", 10, 15),
+                new Film("", "", 3, 6)
+        };
+
+        int expectedProfit = 8;
+        int actualProfit = Manager.calculateProfit(contentList);
+
+        if (expectedProfit != actualProfit) Assert.fail();
+    }
+
+    @Test
+    public void testCalculateExpensesSetIsEmpty() {
+        Content[] contentList = null;
+
+        int expectedExpenses = 0;
+        int actualExpenses = Manager.calculateExpenses(contentList);
+
+        if (expectedExpenses != actualExpenses) Assert.fail();
+    }
+
+    @Test
+    public void testCalculateExpenses() {
+        Content[] contentList = new Content[] {
+                new Film("", "", 10, 15),
+                new Film("", "", 3, 6)
+        };
+
+        int expectedExpenses = 13;
+        int actualExpenses = Manager.calculateExpenses(contentList);
+
+        if (expectedExpenses != actualExpenses) Assert.fail();
+    }
+
+    @Test
+    public void testCheckStatusSetIsEmpty() {
+        Content[] contentList = null;
+
+        String expectedStatus = "";
+        String actualStatus = Manager.checkStatus(contentList);
+
+        if (!expectedStatus.equals(actualStatus)) Assert.fail();
+    }
+
+    @Test
+    public void testCheckStatusIsFailed() {
+        Content[] contentList = new Content[] {new Film("noname", "", 10, 5)};
+
+        String expectedStatus = "noname was failed\n";
+        String actualStatus = Manager.checkStatus(contentList);
+
+        if (!expectedStatus.equals(actualStatus)) Assert.fail();
+    }
+
+    @Test
+    public void testCheckStatusIsSuccessful() {
+        Content[] contentList = new Content[] {new Film("noname", "", 5, 10)};
+
+        String expectedStatus = "noname was successful\n";
+        String actualStatus = Manager.checkStatus(contentList);
+
+        if (!expectedStatus.equals(actualStatus)) Assert.fail();
+    }
+}
